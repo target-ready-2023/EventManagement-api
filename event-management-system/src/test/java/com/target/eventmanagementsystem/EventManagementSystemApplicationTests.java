9a8e8e13c2d6f4c0cd537e32d1a7adab85a3a0be
Sakshi Thawal <72642308+SakshiThawal@users.noreply.github.com>
2023-07-18 20:51:52 +0530

    Add files via upload

---
+++ event-management-system/src/test/java/com/target/eventmanagementsystem/EventManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.target.eventmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
