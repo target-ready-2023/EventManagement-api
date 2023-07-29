76c3e8cf6af475726d98b9d76c6ef37243ddd603
Amogh <107115276+AmoghN-08@users.noreply.github.com>
2023-07-29 16:06:56 +0530

    Configure .gitignore file
    Configured .gitignore to ignore user specific settings and build output

--- event-management-system/src/main/java/com/target/eventmanagementsystem/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.target.eventmanagementsystem.repository;
-
-import com.target.eventmanagementsystem.models.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users,Integer> {
-
-}
